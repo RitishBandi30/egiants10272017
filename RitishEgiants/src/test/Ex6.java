5e812010fc4b993023c5b05affb3583ffd3c3f02
RitishBandi30 <S525701@mail.nwmissouri.edu>
2017-10-27 13:53:45 -0500

    Initial Commit

---
+++ RitishEgiants/src/test/Ex6.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+import java.util.Arrays;
+
+
+/**
+ *
+ * @author Ritish
+ */
+public class Ex6 {
+    public static void main(String[] args) {
+        int i=0, j=0;
+        try{
+            j/=i;
+        }
+        catch(Exception e){
+            System.out.println("error");
+        }
+
+    }
+}
